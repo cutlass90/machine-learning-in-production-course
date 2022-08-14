9c67f79cfe084eecd470d70857559fe7e4118560
cutlass90 <cutlass900@gmail.com>
2022-08-13 20:24:56 -0400

    clean up

---
+++ test_task/README.md
@@ -0,0 +1,9 @@
+## Solution for test task for Machine Learning in Production course
+
+### Train
+To train model simply run train.py script.
+```angular2html
+python train.py
+```
+trained model will be saved into ```checkpoint``` dir
+<br>Evaluation score RMSE=0.57
--- test_task/train.py
+++ test_task/train.py
@@ -1,32 +1,53 @@
+import pickle
+from os.path import join
+
 import numpy as np
 import pandas as pd
+import scipy
+from sklearn.metrics import mean_squared_error
 from torch.utils.data import Dataset
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 from transformers import AutoTokenizer
-from os.path import join
-import pickle
-import scipy
 from transformers import EarlyStoppingCallback, IntervalStrategy
-from sklearn.metrics import mean_squared_error
-
 
 
 class TextDataset(Dataset):
-    def __init__(self, df, values2label_func, tokenizer):
+    def __init__(self, df, bins, tokenizer):
         self.is_train = 'target' in df.columns
         self.data = df
         self.encodings = tokenizer(df.excerpt.tolist(), truncation=True, padding=True)
-        self.values2label = values2label_func
+        self.bins = bins
 
     def __getitem__(self, item):
         sample = {key: val[item] for key, val in self.encodings.items()}
         if self.is_train:
-            sample['labels'] = int(self.values2label([self.data.target.values[item]])[0])
+            sample['labels'] = int(values2label([self.data.target.values[item]], self.bins)[0])
 
         return sample
 
     def __len__(self):
         return len(self.encodings.encodings)
+
+
+def values2label(list_values, bins):
+    return np.digitize(list_values, bins) - 1
+
+
+def vector2prediction(x, bins):
+    x = scipy.special.softmax(x, axis=1)
+    y = np.array([(bins[i] + bins[i + 1]) / 2 for i in range(len(bins) - 1)] + [bins.max()])
+    return (x * y[None, :]).sum(1)
+
+
+def compute_metrics(p):
+    with open('bins', 'rb') as f:
+        bins = pickle.load(f)
+    pred = vector2prediction(p.predictions, bins)
+    b = np.zeros((p.label_ids.size, p.predictions.shape[1]))
+    b[np.arange(p.label_ids.size), p.label_ids] = 1
+    targets = vector2prediction(b * 1e3, bins)
+    rms = mean_squared_error(targets, pred, squared=False)
+    return {"rmse": rms}
 
 
 def main(path2data='data', train_part=0.8):
@@ -35,31 +56,17 @@ def main(path2data='data', train_part=0.8):
 
     train_path = join(path2data, 'train.csv')
     train_df = pd.read_csv(train_path)
-    bins = np.histogram(train_df.target, bins=N_classes-1)[1]
-    def values2label(list_values):
-        return np.digitize(list_values, bins)-1
-    def vector2prediction(x):
-        x = scipy.special.softmax(x, axis=1)
-        y = np.array([(bins[i] + bins[i+1])/2 for i in range(len(bins)-1)] + [bins.max()])
-        return (x*y[None, :]).sum(1)
+    bins = np.histogram(train_df.target, bins=N_classes - 1)[1]
+    with open('bins', 'wb') as f:
+        pickle.dump(bins, f)
 
-    def compute_metrics(p):
-        pred = vector2prediction(p.predictions)
-        b = np.zeros((p.label_ids.size, N_classes))
-        b[np.arange(p.label_ids.size), p.label_ids] = 1
-        targets = vector2prediction(b*1e3)
-        rms = mean_squared_error(targets, pred, squared=False)
-        return {"rmse": rms}
-
-    ind = int(len(train_df)*train_part)
-    train_dataset = TextDataset(train_df[:ind], values2label, tokenizer)
-    val_dataset = TextDataset(train_df[ind:], values2label, tokenizer)
+    ind = int(len(train_df) * train_part)
+    train_dataset = TextDataset(train_df[:ind], bins, tokenizer)
+    val_dataset = TextDataset(train_df[ind:], bins, tokenizer)
 
     test_path = join(path2data, 'test.csv')
     test_df = pd.read_csv(test_path)
-    test_dataset = TextDataset(test_df, values2label, tokenizer)
-
-
+    test_dataset = TextDataset(test_df, bins, tokenizer)
 
     model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=N_classes)
     training_args = TrainingArguments(
