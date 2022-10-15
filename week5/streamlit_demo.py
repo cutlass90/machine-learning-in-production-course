9d4a6d6f27dc4615009a93483c1cdec0e9434071
cutlass90 <cutlass900@gmail.com>
2022-10-15 16:51:48 -0400

    streamlit app that do nothing. just page

---
+++ week5/streamlit_demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+DEVICE = 'cuda:0'
+
+
+def main():
+    st.header('You Can Be Anyone You Want')
+    st.image('static/a_portrait.png')
+
+
+if __name__ == '__main__':
+    main()
