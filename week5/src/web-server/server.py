0b3540ad41f0fb3cbc9cc1ff414d68dd2246c220
cutlass90 <cutlass900@gmail.com>
2022-10-29 16:21:52 -0400

    save

--- week5/src/web-server/server.py
+++ week5/src/web-server/server.py
@@ -9,6 +9,7 @@ from google.cloud import storage
 
 from config import opt
 
+client = aiplatform.gapic.JobServiceClient(client_options={"api_endpoint": "us-central1-aiplatform.googleapis.com"})
 
 def write_image(source_file, destination_blob_name):
     storage_client = storage.Client(opt.project_name)
@@ -17,33 +18,22 @@ def write_image(source_file, destination_blob_name):
     blob.upload_from_file(source_file)
 
 
-def create_custom_job_sample(
-        project: str,
-        display_name: str,
-        container_image_uri: str,
-        location: str = "us-central1",
-        api_endpoint: str = "us-central1-aiplatform.googleapis.com",
-        args=[]
-):
-    # The AI Platform services require regional API endpoints.
-    client_options = {"api_endpoint": api_endpoint}
-    # Initialize client that will be used to create and send requests.
-    # This client only needs to be created once, and can be reused for multiple requests.
-    client = aiplatform.gapic.JobServiceClient(client_options=client_options)
+def create_custom_job_sample(project: str, display_name: str, container_image_uri: str, location: str = "us-central1", args=()):
     custom_job = {
         "display_name": display_name,
         "job_spec": {
             "worker_pool_specs": [
                 {
                     "machine_spec": {
-                        "machine_type": "a2-highgpu-1g",
-                        "accelerator_type": aiplatform.gapic.AcceleratorType.NVIDIA_TESLA_A100,
-                        "accelerator_count": 1,
+                        "machine_type": "e2-standard-4",
+                        # "machine_type": "a2-highgpu-1g",
+                        # "accelerator_type": aiplatform.gapic.AcceleratorType.NVIDIA_TESLA_A100,
+                        # "accelerator_count": 1,
                     },
                     "replica_count": 1,
                     "container_spec": {
                         "image_uri": container_image_uri,
-                        "command": ["python train.py "],
+                        # "command": ["python train.py "],
                         "args": args,
                     },
                 }
@@ -64,13 +54,17 @@ async def strarttraining(images_list: List[UploadFile] = File(...)):
     blob_names = [str(user_id) + f'_img{i}' + os.path.splitext(img.filename)[1].lower() for i, img in enumerate(images_list)]
     for i, (img, blob) in enumerate(zip(images_list, blob_names)):
         write_image(img.file, blob)
-    args = ['--checkpoint-name', str(user_id) + 'checkpoint.ckpt', '--imgs-list'] + blob_names
+    args = ['--checkpoint-name ' + str(user_id) + 'checkpoint.ckpt',
+            '--imgs-list ' + ' '.join(blob_names)]
+    args = ["--checkpoint-name=sdaklfnljksdh.ckpt"]
+
+    print('args', args)
     create_custom_job_sample(
         project=opt.project_name,
         display_name='train' + str(user_id),
         container_image_uri='gcr.io/sd-concept-project/sd-concept-train-job:latest',
         location="us-central1",
-        api_endpoint="us-central1-aiplatform.googleapis.com",
+        # api_endpoint="us-central1-aiplatform.googleapis.com",
         args=args
     )
 
