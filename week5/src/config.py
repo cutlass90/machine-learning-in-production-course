749305409251a1397a8884a2dc8f6c19ee2797b7
cutlass90 <cutlass900@gmail.com>
2022-11-06 14:56:31 -0500

    save custom job waiting for quotas

--- week5/src/config.py
+++ week5/src/config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import os
 
-DEBUG = False
+DEBUG = True
 
 @dataclass
 class Config:
@@ -9,16 +9,21 @@ class Config:
     #general
     debug = DEBUG
     project_name = 'sd-concept-project'
+    region = 'us-central1'
+    checkpoints_storage_url = 'gs://sd-concept-checkpoints-storage'
 
     #buckets
     checkpoints_bucket_name = 'sd-concept-checkpoints-storage'
     results_bucket_name = 'sd-concept-results'
 
     #training
+    training_container_uri = 'gcr.io/sd-concept-project/sd-concept-train-job:latest'
     dataset = 'person'
     token = 'NazarShmatko'
     training_images_folder = 'training_images'
     max_training_steps = 20 if DEBUG else 2000
+    machine_type = "a2-highgpu-1g"
+    accelerator_type = "NVIDIA_TESLA_A100"
 
     #potrs
     host_ip = "172.17.0.1" if DEBUG else os.environ['internal-url']
---
+++ week5/src/train-job/custom job playground.py
@@ -0,0 +1,37 @@
+from google.cloud import aiplatform
+from config import opt
+from typing import List
+
+def custom_training_job_sample(
+    project: str,
+    location: str,
+    bucket: str,
+    display_name: str,
+    container_uri: str,
+    replica_count: int,
+    command: List[str]
+):
+    aiplatform.init(project=project, location=location, staging_bucket=bucket)
+
+    job = aiplatform.CustomContainerTrainingJob(
+        display_name=display_name,
+        container_uri=container_uri,
+        command=command
+    )
+
+    model = job.run(
+        replica_count=replica_count, machine_type=opt.machine_type, accelerator_type=opt.accelerator_type,
+        accelerator_count=1, sync=True
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+    custom_training_job_sample(project=opt.project_name,
+                               location=opt.region,
+                               bucket=opt.checkpoints_storage_url,
+                               display_name='display_name_nazar',
+                               container_uri=opt.training_container_uri,
+                               replica_count=1,
+                               command=["python", "train.py", "--checkpoint-name", "testcustomjob.ckpt", "--imgs-list", "1.jpg", "2.jpg", "3.jpg"])
